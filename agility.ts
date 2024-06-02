a7f0c66e8b3dd689632183b6243de25261a60108
bobcermak <145228912+bobcermak@users.noreply.github.com>
2024-06-02 18:07:21 +0200

    add more efficient + add color recognition

--- agility.ts
+++ agility.ts
@@ -17,11 +17,10 @@ function blackLine(): void {
             TPBot.stopCar()
         }
         if (TPBot.trackLine(TPBot.TrackingState.L_R_line) && !firstObserve) {
-            TPBot.setTravelSpeed(TPBot.DriveDirection.Forward, 60)
+            TPBot.setTravelSpeed(TPBot.DriveDirection.Forward, 40)
         }
         if (!TPBot.trackLine(TPBot.TrackingState.L_R_line) && !firstObserve) {
-            TPBot.setWheels(80, -80)
-            basic.pause(400)
+            TPBot.setWheels(40, -40)
         }
     }
 }
@@ -35,34 +34,74 @@ function sonar() {
 
 //find balls - colors
 function findBalls(): string {
+    let blueCount: number = 0
+    let redCount: number = 0
+
     let statusBall: string
-    PlanetX_AILens.cameraImage()
-    if (PlanetX_AILens.ballColor(PlanetX_AILens.ballColorList.Blue)) statusBall = "B"
-    else if (PlanetX_AILens.ballColor(PlanetX_AILens.ballColorList.Red)) statusBall = "R"
-    else statusBall = "N"
+
+    for (let i = 0; i < 12; i++) {
+        PlanetX_AILens.cameraImage()
+        if (PlanetX_AILens.ballColor(PlanetX_AILens.ballColorList.Blue)) blueCount++  
+        else if (PlanetX_AILens.ballColor(PlanetX_AILens.ballColorList.Red)) redCount++ 
+    }
+    if (blueCount > redCount && blueCount >= 3) statusBall = "B"
+    else if (redCount > blueCount && redCount >= 3) statusBall = "R"
+    
     return statusBall
 }
 
+
+
 //to observe ball
 function toObserve(): boolean {
-    PlanetX_AILens.cameraImage()
-    return PlanetX_AILens.checkBall()
+    let ballCount: number = 0
+
+    for (let i = 0; i < 12; i++) {
+        PlanetX_AILens.cameraImage()
+        if (PlanetX_AILens.checkBall()) ballCount++
+    }
+
+    return ballCount >= 3
 }
+
 
 //find cards
 function findCards(): string {
+    let blueCardCount: number = 0
+    let redCardCount: number = 0
+
     let statusCard: string
-    if (PlanetX_AILens.colorCheck(PlanetX_AILens.ColorLs.blue)) statusCard = "B"
-    else if (PlanetX_AILens.colorCheck(PlanetX_AILens.ColorLs.red)) statusCard = "R"
+
+    for (let i = 0; i < 12; i++) {
+        PlanetX_AILens.cameraImage()
+        if (PlanetX_AILens.colorCheck(PlanetX_AILens.ColorLs.blue)) blueCardCount++
+        else if (PlanetX_AILens.colorCheck(PlanetX_AILens.ColorLs.red)) redCardCount++
+    }
+    if (blueCardCount > redCardCount && blueCardCount >= 3) statusCard = "B"
+    else if (redCardCount > blueCardCount && redCardCount >= 3) statusCard = "R"
     else statusCard = "N"
+
     return statusCard
 }
+
+
 
 //MAIN FUNCTIONS
 
 //driving
+let blue: number = 0
+let red: number = 0
+
 function driving(): void {
     blackLine()
-    if (firstObserve) basic.showString("Hello!")
+    if (firstObserve) {  //sorting between blue and red
+        for (let i = 0; i < 3; i++) {
+            findBalls()
+            if (findBalls() === "B") blue++
+            else red++
+        }
+        if (blue > red) basic.showString("B")
+        else basic.showString("R")
+    }
     //catching function + servo
 }
--- main.ts
+++ main.ts
@@ -11,5 +11,4 @@ driving()
 
 
 
-
 //ENDING
